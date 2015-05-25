cc1260dcd03c052b35d36302a49b86fede02bbdd
Daniel Hoang <thesubway@gmail.com>
2015-05-25 12:18:09 -0700

    Add feature to skip any lines with comments.

--- myTranslator.pl
+++ myTranslator.pl
@@ -1,16 +1,26 @@
 #open(my $FileHandle, "<", "tablets.csv") or die "Not able to open file";
 open FILE, "tablets.csv" or die $!;
+open FILE2, ">tablets_W1023475.html" or die $!;
 if (FILE) {
-	print("file exists\n");
+	print("file1 exists\n");
 }
 else {
-	print("file does not exist\n");
+	print("file1 does not exist\n");
+}
+if (FILE2) {
+	print("file2 exists\n");
+}
+else {
+	print("file2 does not exist\n");
 }
 $output = "output.txt";
 while (<FILE>) {
-  print $_;
+  $currentLine = $_;
+  $first = substr($currentLine, 0, 1);
+  if ($first ne "#") {
+  	print FILE2 $currentLine." is a line"."\n";
+  }
 }
-# while (<>) {
-# print "This is output file: \n\n";
-# 	print ();
-# }
+print "\n";
+close(FILE);
+close FILE2;
