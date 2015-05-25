18394ef0629bc3f94c7ac5e6d20de70464d5c425
Daniel Hoang <thesubway@gmail.com>
2015-05-25 15:36:14 -0700

    Divide field entries from csv file, and display them separately one line at a time.

--- myTranslator.pl
+++ myTranslator.pl
@@ -1,24 +1,27 @@
 #open(my $FileHandle, "<", "tablets.csv") or die "Not able to open file";
+sub removeFrontWhiteSpace {
+}
 open FILE, "tablets.csv" or die $!;
 open FILE2, ">tablets_W1023475.html" or die $!;
-if (FILE) {
-	print("file1 exists\n");
+sub leftTrim {
+	$param1 = @_[0];
+	$param1 =~ s/^\s+//;
+	return $param1;
 }
-else {
-	print("file1 does not exist\n");
-}
-if (FILE2) {
-	print("file2 exists\n");
-}
-else {
-	print("file2 does not exist\n");
-}
+# $test1 = " Hello.";
+# $test1 = leftTrim($test1);
+# print "test".$test1;
 $output = "output.txt";
 while (<FILE>) {
   $currentLine = $_;
   $first = substr($currentLine, 0, 1);
   if ($first ne "#") {
-  	print FILE2 $currentLine." is a line"."\n";
+  	print FILE2 $currentLine."\n";
+  	@fields = split(',',$currentLine);
+  	$fields[0] = leftTrim($fields[0]);
+  	$fields[1] = leftTrim($fields[1]);
+  	$fields[2] = leftTrim($fields[2]);
+  	print FILE2 $fields[0]."\n".$fields[1]."\n".$fields[2];
   }
 }
 print "\n";
